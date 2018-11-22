f0d87df065e6887625965f9401afd8e9db385ef5
todorpanev <toshopanev@gmail.com>
2018-11-23 01:28:26 +0200

    setting up grid

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,66 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img src="./assets/Airwaves.svg" class="airwaves" alt="airwaves graphic">
+    <img src="./assets/Logo.svg" class="head-logo" alt="lockedin logo">
+    <BeyondPlayer />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BeyondPlayer from './components/BeyondPlayer.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    BeyondPlayer
   }
 }
 </script>
 
 <style lang="scss">
+
+body,
+html {
+  padding: 0;
+  margin: 0;
+  overflow-x: hidden;
+  overflow-y: scroll;
+
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: grid;
+  justify-content: center;
+  grid-template-columns: repeat(12, 60px);
+  grid-column-gap: 20px; 
 }
+
+.head-logo {
+  grid-column: 1 / 13;
+  justify-self: center;
+}
+
+.airwaves {
+  position: absolute;
+  left: 50%;
+  transform: translateX(-50%);
+  top: -1550px;
+  z-index: -1;
+  animation: airani linear 15s infinite;
+  transform-origin: 0%;
+}
+
+/*@keyframes airani {
+  0% {
+    transform: scale(1) translateX(-50%);
+  }
+
+  100% {
+    transform: scale(2) translateX(-50%);
+  }
+}
+*/
 </style>
---
+++ src/components/BeyondPlayer.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <audio ref="bdPlayer" id="bdplayer" src="http://bassdrive.radioca.st:80/;stream/1" volume="0.1">
+    </audio>
+    <button @click="bdPlay">play/pause</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BeyondPlayer',
+  methods: {
+    bdPlay() {
+      this.$refs.bdPlayer.play();
+    },
+    bdPause() {
+      this.$refs.bdPlayer.pause();
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
